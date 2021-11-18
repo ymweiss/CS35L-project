39415b2c54a964aebda5b93493f70a2fd40c72d5
Noah Gordon <90871515+scratch-johnson@users.noreply.github.com>
2021-11-17 16:49:21 -0800

    create class LoginForm to manage login
    LoginForm currently renders a form with
    2 text boxes for email/password and button
    to submit information.
    
    Button currently doesn't do much.

--- express-static-serve/react-app/src/index.js
+++ express-static-serve/react-app/src/index.js
@@ -2,4 +2,61 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class LoginForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		const targetName = event.target.name
+		if (targetName === "email") {
+			this.setState({
+				email: event.target.value,
+				password: this.state.password,
+			});
+		}
+		else if (targetName === "email") {
+			this.setState({
+				email: this.state.email,
+				password: event.target.value,
+			});
+		}
+	}
+
+	handleSubmit(event) {
+		alert("login was submitted: ");
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					email:
+					<input 
+						name="email"
+						value={this.state.email} 
+						onChange={this.handleChange} 
+					/>
+				</label>
+				<label>
+					password:
+					<input 
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+				</label>
+				<input type="Submit" value="Submit" />
+			</form>
+		);
+	}
+}
+
+ReactDOM.render(<LoginForm />, document.getElementById("root"));
+
