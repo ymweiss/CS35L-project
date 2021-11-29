23d15d47001efbbd6bbecb26fef90e2063b3094e
narinehzargarian <48963755+narinehzargarian@users.noreply.github.com>
2021-11-28 23:41:11 -0800

    The Home page is getting more features
    Now there is a shopping cart with the name of the items, their quantity, and price. Users can also view their total.

---
+++ express-static-serve/react-app/src/Home.js
@@ -0,0 +1,222 @@
+import React, {useState, useEffect} from "react";
+import Axios from "axios";
+//import BrowserRouter from "react-router-dom";
+import "./Home.css";
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+            itemList:[],
+            
+            shopList: [],
+            isCartUpdated: false,
+            isCatSelected: false
+        }
+        this.getList = this.getList.bind(this);
+        this.addToShoppingList = this.addToShoppingList.bind(this);
+        this.displayCart = this.displayCart.bind(this);
+        this.cleanCartHistory = this.cleanCartHistory.bind(this);
+    }
+
+    componentDidMount(){
+        Axios.get("http://localhost:3001/cart")
+        .then((response) => {
+            this.setState({
+                
+                shopList: response.data,
+            });
+        })
+    }
+
+    displayCart(){
+        Axios.get("http://localhost:3001/cart")
+        .then((response) => {
+            this.setState({
+                
+                shopList: response.data,
+            });
+        })
+    }
+
+    addToShoppingList(props){
+        //this.setState({shopList: props});
+        Axios.post("http://localhost:3001/shopping", 
+            {props}
+        ).then((response) => {
+            if(response.data){
+                this.setState({
+                    isCartUpdated: true,
+                });
+                this.displayCart();
+            }
+        })
+        
+
+    }
+
+    cleanCartHistory(){
+        Axios.get("http://localhost:3001/deletecart")
+        .then((response) => {
+            if(response){
+                this.setState({
+                    shopList: [],
+                });
+            }
+        });
+        
+
+    }
+
+    getList(props){
+
+            if(this.state.isCatSelected){
+                this.setState({
+                    isCatSelected: !this.state.isCatSelected,
+                    itemList: []
+                })
+            }
+            
+            else{
+                Axios.post("http://localhost:3001/categories", 
+            
+                {category: props })
+
+                .then((Response) => {
+                    this.setState(
+                        {
+                            isCatSelected: !this.state.isCatSelected,
+                            itemList: Response.data
+                        }
+                    );
+                    
+                });
+            }
+                
+        }
+
+    
+
+    render(){
+        let rows  = [];
+        for(let i = 0; i < this.state.itemList.length; i++){
+            rows.push(
+            <button className ="items" 
+                key="{i}" 
+                onClick={() => 
+                    {this.addToShoppingList(this.state.itemList[i])}
+                }>
+                {this.state.itemList[i].name}   {this.state.itemList[i].price}
+            </button>);
+        }
+
+        let cartRows = []
+        let Total = 0;
+        
+        for(let i = 0; i < this.state.shopList.length; i++){
+            Total += this.state.shopList[i].Price;
+            cartRows.push(
+                <tr key="{i}">
+                    <td key="{i}">{this.state.shopList[i].Name}</td>
+                    <td key="{i}">{this.state.shopList[i].Quantity}</td>
+                    <td key="{i}">${this.state.shopList[i].Price}</td>
+                </tr>
+            );
+        }
+
+        return(
+            <div >
+                <div class="div-1">
+                    Let's QuickShop
+                </div>
+
+                <div class="cart">
+                    <h3>
+                        Shopping Cart
+                    </h3>
+                    <table className="table">
+                        <tr>
+                            <th>Item</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                        </tr>
+                        {cartRows}
+                        
+                    </table>
+                
+                    <p>Total: ${Total.toFixed(2)}</p>
+                    <button className="clear" onClick={this.cleanCartHistory}>Remove All</button>
+
+                </div>
+                
+                <div className="categories">
+                    Shopping Categories
+                    {this.state.isCatSelected? null: 
+                        <button className="button"
+                            onClick={ () => {
+                                    this.getList("produce");
+                                }
+                            }
+                            > 
+                            Produce
+                        </button>}
+
+                    {this.state.isCatSelected? null: 
+                        <button className= "button" 
+                            onClick={() => {
+                                    this.getList("dairy")
+                                }
+                            }>
+                            Dairy & Egg
+                        </button>
+                    }
+                    {this.state.isCatSelected? null: 
+                        <button className= "button" onClick= {()=>
+                            this.getList("meat")
+                        }>
+                            Meat
+                        </button>
+                    }
+                    
+                    {this.state.isCatSelected? null: 
+                        <button className= "button" onClick = {
+                            () => {
+                                this.getList("pantry")
+                            }
+                        }>
+                            Pantry Essential
+                        </button>
+                    }
+                    {this.state.isCatSelected? null: 
+                        <button className= "button" onClick={() => {
+                            this.getList("bakery")
+                        }}>
+                            Bakery
+                        </button>
+                    }
+                    <div>
+                        {this.state.isCatSelected? rows: null}
+                    </div>
+                    
+                    
+                    {!this.state.isCatSelected? null:
+                            <button className="back-button" onClick={
+                                () => {this.setState({
+                                    isCatSelected: false,
+                                    isCartUpdated: false,
+                                })}
+                            }>
+                                Back
+                            </button>
+                        }
+                        
+                </div>
+               
+            </div>
+           
+        );
+    }
+}
+
+export default Home;
