d1ce1b4b8923425ebc505af29bf7b7fe9e047d05
narinehzargarian <48963755+narinehzargarian@users.noreply.github.com>
2021-11-30 14:40:54 -0800

    Exit page is added to the components

---
+++ express-static-serve/react-app/src/Exit.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Exit.css";
+
+function Exit(){
+    return (
+        <div class="exit">
+            <h1 >Thank You for QuickShopping!</h1>
+        </div>
+    
+    );
+}
+export default Exit;
