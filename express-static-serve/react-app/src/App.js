d68e5ddd88adc00f05f640f2c9a8534789b4f7be
narinehzargarian <48963755+narinehzargarian@users.noreply.github.com>
2021-11-30 14:40:05 -0800

    App.js now navigates between Home and Exit

--- express-static-serve/react-app/src/App.js
+++ express-static-serve/react-app/src/App.js
@@ -1,37 +1,27 @@
-import React from "react";
-import axios from "axios";
-import "./styles.css";
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router,
+    Routes,
+    Route,
+    Link} from "react-router-dom";
+import Exit from "./Exit";
+import Home from "./Home";
 
-export default class App extends React.Component {
-  state = {
-    users: [],
-  };
-  componentDidMount() {
-    axios.get("/users.json").then((response) => {
-      this.setState({ users: response.data });
-    });
-  }
-
-  render() {
-    const { users } = this.state;
-    return (
-      <div>
-        <ul className="users">
-          {users.map((user) => (
-            <li className="user">
-              <p>
-                <strong>Name:</strong> {user.name}
-              </p>
-              <p>
-                <strong>Email:</strong> {user.email}
-              </p>
-              <p>
-                <strong>City:</strong> {user.address.city}
-              </p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <Router>
+                <Routes>
+                    <Route exact path ="/" element={<Home/>}></Route>
+                    <Route exact path="/exit" element={<Exit/>}></Route>
+                </Routes>
+                
+            </Router>
+            
+        );
+    }
+    
 }
+export default App;
