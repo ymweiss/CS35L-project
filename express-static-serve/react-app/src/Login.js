5e5a7cbbda56b2f100c3bce5113ff9b1300ebb08
Noah Gordon <90871515+scratch-johnson@users.noreply.github.com>
2021-11-30 17:26:08 -0800

    Add src files to merge + change Login
    add changes from cd4831558606ab0ca7b8d2b1cee6cbf6c9ccd91f
    
    Make Login server communication global function based.

--- express-static-serve/react-app/src/Login.js
+++ express-static-serve/react-app/src/Login.js
@@ -1,71 +1,202 @@
-import React, {useState, useEffect} from "react";
+import React, {createContext, useContext} from "react";
 //import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Axios from "axios";
 import "./Login.css";
 import Home from "./Home";
 import ReactDOM from "react-dom";
+/*import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+*/
 
+export class MainPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showLogin: false,
+			showRegister: false,
+		};
+		this.loginBtnClick = this.loginBtnClick.bind(this);
+		this.registerBtnClick = this.registerBtnClick.bind(this);
+	}
 
-function Login(){
+	loginBtnClick() {
+		this.setState ({
+			showLogin: true,
+			showRegister: this.state.showRegister,
+		});
+	}
 
-  const [searchUser, setSearchUser] = useState("");
-  const [searchPass, setSearchPass] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+	registerBtnClick() {
+		this.setState ({
+			showLogin: this.state.showLogin,
+			showRegister: true,
+		});
+	}
 
+	render() {
+		return (
+			<div>
+				{!this.state.showRegister && !this.state.showLogin ?
+				<button onClick={this.loginBtnClick}>login</button> :
+				null}
+				{this.state.showLogin ? <LoginForm /> : null}
 
-  const submitInfo = () =>{
-      Axios.post("http://localhost:3001/login/", {
-      username : searchUser,     
-      })
-      .then((response) => {
-        
-          if(response){
-            setIsLoggedIn(true);
+				{!this.state.showLogin && !this.state.showRegister ?
+				<button onClick={this.registerBtnClick}>register</button> :
+				null}
+				{this.state.showRegister ? <RegisterForm /> : null}
 
-            setUsername(response.data.username);
-            setPassword(response.data.password);
-            
-          }
-        
-      });
-      
+				<Home />
+			</div>
+		);
+	}
+}
 
-  };
+export const handleLogin = async values => {
+	Axios.post("http://localhost:3001/login", {
+		username: values.user,
+		password: values.password,
+	}).then((response) => {
+		if (response.data.isLoggedIn) {
+			alert("logged in");
+		}
+		else {
+			alert("not a valid username or password");
+		}
+	});
+}
 
-  return(
-    <div className="login">
-        <h1><i>QuickShop</i></h1>
-        <div className="form">
-            <label for="username" >Username: </label> 
-            <input 
-              type="text"
-              name="username"
-              onChange={(v) => {
-                setSearchUser(v.target.value);
-              }}
-              />
-
-            <label for="password" > Password (8 characters or more):</label> 
-            <input 
-              type="password"
-              name="password"
-              minLength="8"
-              required
-              onChange={(v)=>{
-                  setSearchPass(v.target.value);
-              }
-            }
-              />
-            
-            <button onClick={submitInfo} type="submit">Login</button>
-            
-        </div>
-      </div>
-  );
+export const handleRegister = async values => {
+	Axios.post("http://localhost:3001/register", {
+		username: values.user,
+		password: values.password,
+	}).then((response) => {
+		if (!response.body) {
+			alert("account added");
+		}
+		else {
+			alert("user already exists");
+		}
+	});
 }
 
 
-export default Login;
+export class LoginForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: "",
+			password: "",
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		const targetName = event.target.name
+		if (targetName === "user") {
+			this.setState({
+				user: event.target.value,
+				password: this.state.password,
+			});
+		}
+		else if (targetName === "password") {
+			this.setState({
+				user: this.state.user,
+				password: event.target.value,
+			});
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<label>
+					user:
+					<input 
+						name="user"
+						onChange={this.handleChange}
+						value={this.state.user}
+					/>
+				</label>
+				<label>
+					password:
+					<input 
+						name="password"
+						onChange={this.handleChange}
+						value={this.state.password}
+					/>
+				</label>
+				<button onClick={() => {
+					const {user, password} = this.state;
+					handleLogin({user, password});
+				}}>
+					Login
+				</button>
+			</div>
+		);
+	}
+}
+
+export class RegisterForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: "",
+			password: "",
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		const targetName = event.target.name
+		if (targetName === "user") {
+			this.setState({
+				user: event.target.value,
+				password: this.state.password,
+			});
+		}
+		else if (targetName === "password") {
+			this.setState({
+				user: this.state.user,
+				password: event.target.value,
+			});
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<label>
+					user:
+					<input 
+						name="user"
+						onChange={this.handleChange} 
+						value={this.state.user}
+					/>
+				</label>
+				<label>
+					password:
+					<input 
+						name="password"
+						onChange={this.handleChange}
+						value={this.state.password}
+					/>
+				</label>
+				<button onClick={() => {
+					const {user, password} = this.state;
+					handleRegister({user, password});
+				}}>
+					Register
+				</button>
+			</div>
+		);
+	}
+}
+
 
