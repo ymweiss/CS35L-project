a3f0e97e7b7c8c8819d1b8767e665dc37438fe26
Noah Gordon <90871515+scratch-johnson@users.noreply.github.com>
2021-11-30 17:28:33 -0800

    merge changes to server
    Merge changes from cd4831558606ab0ca7b8d2b1cee6cbf6c9ccd91f
    with current Login, Register functionality.

--- express-static-serve/react-app/server/index.js
+++ express-static-serve/react-app/server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "kn1ght21", //use password for your mysql database
+    password: "", //use password for your mysql database
     database: "shop_app"
 });
 
@@ -82,6 +82,214 @@ app.post("/register", (req, res) => {
 			}
 
     });
+});
+
+
+app.post("/shopping", 
+    (req, res) => {
+        console.log(req.body);
+        const id = req.body.props.id;
+        const name = req.body.props.name;
+        const price = req.body.props.price;
+
+        const stmnt = "INSERT INTO shopcart VALUES (?, ?, ?)";
+        db.query(stmnt, [id, name, price], (err, result) => {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(result);
+                res.send(result);
+            }
+        })
+
+    }
+);
+
+app.post("/increase", (req, res) => {
+    const id = req.body.props.id;
+    const name = req.body.props.Name;
+    const stmnt = "SELECT * FROM shopcart " + 
+        "WHERE id = ? and name= ?";
+    db.query(stmnt, [id, name], (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(result){
+                db.query("INSERT INTO shopcart VALUES (?, ?, ?)", [result[0].id, result[0].name, result[0].price] , 
+                (err_1, result_1) => {
+                    if(err){
+                        console.log(err_1);
+                    }
+                    else {
+                        res.send(result_1);
+                    }
+                });
+            }
+            
+        }
+        
+    });
+});
+
+app.post("/decrease", (req, res) => {
+    const id = req.body.props.id;
+    const name = req.body.props.Name;
+    console.log(req.body);
+    const stmnt = "DELETE FROM shopcart " + 
+        "WHERE id = ? and name = ? " + 
+        "LIMIT 1";
+    db.query(stmnt, [id, name], (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(result);
+        }
+    });
+});
+
+app.get("/deletecart", (req, res) => {
+    const stmnt = "DELETE FROM shopcart";
+    db.query(stmnt, (err, result) => {
+        if(err){
+            console.log(err);
+        
+        }
+        else{
+            console.log(result);
+            res.send(result);
+        }
+        
+    });
+});
+
+app.post("/search", 
+    (req, res) => {
+        let cat = req.body.category;
+        let search = req.body.search.toLowerCase();
+        if(search.length > 0){
+            let result = [];
+            console.log(search, cat);
+
+            const stmnt = "SELECT * FROM " + cat + 
+            " WHERE name LIKE '%" + search + "%'";
+            db.query(stmnt, (err, result) => {
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.send(result);
+                    console.log(result);
+                }
+            })
+            
+        }
+        
+        
+    });
+
+app.get("/cart", 
+    (req, res) => {
+        const stmnt = "SELECT id , name AS Name, COUNT(*) AS Quantity," + 
+        "ROUND(SUM(price), 2) AS Price " +
+        "FROM shopcart " + 
+        "GROUP BY id, name";
+        db.query(stmnt, (err, result) => {
+            if(err){
+                console.log(err);
+
+            }
+            else{
+                
+                res.send(result);
+            }
+        })
+        
+});
+
+app.post("/categories", 
+    (req, res)=>{
+    
+        const categ = req.body.category;
+        console.log(categ);
+
+        if(categ === "produce"){
+            const stmnt = "SELECT * FROM produce" ;
+        
+            db.query(stmnt, (err, result) => {
+                if(err){
+                    console.log("error: ", err);
+                }
+                else{
+                    console.log(result);
+                    res.send(result);
+                }
+                
+            });
+        }
+
+        if(categ === "dairy"){
+            const stmnt = "SELECT * FROM dairy" ;
+        
+            db.query(stmnt, (err, result) => {
+                if(err){
+                    console.log("error: ", err);
+                }
+                else{
+                    console.log(result);
+                    res.send(result);
+                }
+                
+            });
+        }
+
+        if(categ === "meat"){
+            const stmnt = "SELECT * FROM meat" ;
+        
+            db.query(stmnt, (err, result) => {
+                if(err){
+                    console.log("error: ", err);
+                }
+                else{
+                    console.log(result);
+                    res.send(result);
+                }
+                
+            });
+        }
+
+        if(categ === "pantry"){
+            const stmnt = "SELECT * FROM pantry" ;
+        
+            db.query(stmnt, (err, result) => {
+                if(err){
+                    console.log("error: ", err);
+                }
+                else{
+                    console.log(result);
+                    res.send(result);
+                }
+                
+            });
+        }
+
+        if(categ === "bakery"){
+            const stmnt = "SELECT * FROM bakery" ;
+        
+            db.query(stmnt, (err, result) => {
+                if(err){
+                    console.log("error: ", err);
+                }
+                else{
+                    console.log(result);
+                    res.send(result);
+                }
+                
+            });
+        }
+        
 });
 
 
